c01318d30bcd8000b169eb11f1f487090de8cead
naiborab <abrahamnaiborhu@gmail.com>
2023-09-08 08:36:17 +0700

    Initialize project

---
+++ Abraham/src/page/HomePage.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function HomePage() {
+  return (
+    <section>
+            <div className='text-4xl text-center'>Home Page</div>
+
+    </section>
+  )
+}
