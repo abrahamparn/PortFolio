6498a2b754eaa9e41a044c422e82439fa9009aad
naiborab <abrahamnaiborhu@gmail.com>
2023-11-01 16:37:44 +0700

    adding new skillset.jsx

--- Abraham/src/views/Home.jsx
+++ Abraham/src/views/Home.jsx
@@ -3,12 +3,14 @@ import Navbar from '../components/Navbar'
 import Hero from '../parts/Home/Hero'
 import WorkExprience from '../parts/Home/WorkExperience'
 import PhilosophyValue from '../parts/Home/PhilosophyValue'
+import Skillset from '../parts/Home/Skillset'
 export default function Home() {
   return (
-    <section className='grow' >
-      <Hero/>
+    <section className='grow ' >
+      {/* <Hero/> */}
       <WorkExprience/>
       <PhilosophyValue/>
+      <Skillset/>
 
     </section>
   )
