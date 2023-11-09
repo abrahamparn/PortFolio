5f07d94491787e1c64b5b32ed19288d2da4ae7a3
Abraham Naiborhu <87258755+abrahamparn@users.noreply.github.com>
2023-11-09 14:42:34 +0700

    changing the imports

--- Abraham/src/parts/Home/Skillset.jsx
+++ Abraham/src/parts/Home/Skillset.jsx
@@ -1,35 +1,47 @@
-import React from 'react'
-
+import React from "react";
+import kotlin from "../../assets/skillset/kotlin.svg";
+import net from "../../assets/skillset/net.svg";
+import node from "../../assets/skillset/node.svg";
 export default function Skillset() {
   return (
-    <section className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 md:gap-x-5 xl:gap-x-10 text-sen gap-y-10 text-white py-12 px-5'>
-        <div className=' md:row-span-3 flex flex-col gap-4'>
-            <h2 className='text-3xl font-bold'>Skillset</h2>
-            <p className='text-sm text-light_gray'>
-            I could do a lot of things. 
-            Be it Android application, ASP.NET MVC, VB.NET, Electron JS, Machine Learning models, MERN.
-            I have made a lot of projects and currently I am a backend developer. 
-            I am the perfect person to hire when it comes to a full fledged project. 
-            Whatever your needs are, I can pretty much take on any challenge.</p>
-        </div>
+    <section className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 md:gap-x-5 xl:gap-x-10 text-sen gap-y-10 text-white py-12 px-5">
+      <div className=" md:row-span-3 flex flex-col gap-4">
+        <h2 className="text-3xl font-bold">Skillset</h2>
+        <p className="text-sm text-light_gray">
+          I could do a lot of things. Be it Android application, ASP.NET MVC,
+          VB.NET, Electron JS, Machine Learning models, MERN. I have made a lot
+          of projects and currently I am a backend developer. I am the perfect
+          person to hire when it comes to a full fledged project. Whatever your
+          needs are, I can pretty much take on any challenge.
+        </p>
+      </div>
 
-        <div className='Android'>
-            <img src='../src/assets/skillset/kotlin.svg' className='h-8 mb-3'/>
-            <h2 className='text-2xl mb-1'>Android Development</h2>
-            <p className='text-sm text-light_gray'>Working with Tanamin has taught me how to create Android applications using Kotlin. I have also publish the application into the play store.</p>
-        </div>
+      <div className="Android">
+        <img src={kotlin} className="h-8 mb-3" />
+        <h2 className="text-2xl mb-1">Android Development</h2>
+        <p className="text-sm text-light_gray">
+          Working with Tanamin has taught me how to create Android applications
+          using Kotlin. I have also publish the application into the play store.
+        </p>
+      </div>
 
-        <div className='MERN'>
-            <img src='../src/assets/skillset/node.svg' className='h-8 mb-3'/>
-            <h2 className='text-2xl mb-1'>Javascript Fullstack</h2>
-            <p className='text-sm text-light_gray'>Working with waktumerenung provides me with a lot of insights on how to create project using MERN!</p>
-        </div>
+      <div className="MERN">
+        <img src={node} className="h-8 mb-3" />
+        <h2 className="text-2xl mb-1">Javascript Fullstack</h2>
+        <p className="text-sm text-light_gray">
+          Working with waktumerenung provides me with a lot of insights on how
+          to create project using MERN!
+        </p>
+      </div>
 
-        <div className='.NET'>
-            <img src='../src/assets/skillset/net.svg' className='h-8 mb-3'/>
-            <h2 className='text-2xl mb-1'>.NET Developer</h2>
-            <p className='text-sm text-light_gray'>Working with Mattel has taught me on how to create asp.net mvc project and vb.net projects</p>
-        </div>
+      <div className=".NET">
+        <img src={net} className="h-8 mb-3" />
+        <h2 className="text-2xl mb-1">.NET Developer</h2>
+        <p className="text-sm text-light_gray">
+          Working with Mattel has taught me on how to create asp.net mvc project
+          and vb.net projects
+        </p>
+      </div>
     </section>
-  )
+  );
 }
--- Abraham/src/parts/Home/WorkExperience.jsx
+++ Abraham/src/parts/Home/WorkExperience.jsx
@@ -1,53 +1,79 @@
-import React, { useLayoutEffect, useRef }  from 'react'
-import gsap from 'gsap'
-import ScrollTrigger from 'gsap/ScrollTrigger'
-
-gsap.registerPlugin(ScrollTrigger)
+import React, { useLayoutEffect, useRef } from "react";
+import gsap from "gsap";
+import ScrollTrigger from "gsap/ScrollTrigger";
+import one from "../../assets/numbers/01.png";
+import two from "../../assets/numbers/02.png";
+import three from "../../assets/numbers/03.png";
+gsap.registerPlugin(ScrollTrigger);
 export default function WorkExperience() {
-  const number = useRef(null)
+  const number = useRef(null);
 
   useLayoutEffect(() => {
     let ctx = gsap.context(() => {
-      gsap.fromTo(".animateWork", {
-        opacity:0
-      }, {
-        opacity:1,
-        duration:3,
-        stagger: 0.5,
-        scrollTrigger:{
-          trigger: number.current
+      gsap.fromTo(
+        ".animateWork",
+        {
+          opacity: 0,
+        },
+        {
+          opacity: 1,
+          duration: 3,
+          stagger: 0.5,
+          scrollTrigger: {
+            trigger: number.current,
+          },
         }
-      });
-  
-    })
+      );
+    });
     return () => ctx.revert(); // cleanup
-
   }, []);
-  
+
   return (
-    <section  className='flex flex-col px-5 py-16 md:px-10 lg:px-14 xl:px-20 font-sen text-white '>
-        <p className="text-light_gray text-base md:text-xl">WORK EXPERIENCE</p>
-        <p className=' text-3xl md:text-4xl font-bold font-sen'>Experience that I have gained so far.</p>
+    <section className="flex flex-col px-5 py-16 md:px-10 lg:px-14 xl:px-20 font-sen text-white ">
+      <p className="text-light_gray text-base md:text-xl">WORK EXPERIENCE</p>
+      <p className=" text-3xl md:text-4xl font-bold font-sen">
+        Experience that I have gained so far.
+      </p>
 
+      <div className=" py-10 flex flex-col lg:flex-row gap-16" ref={number}>
+        <div className="flex flex-col gap-2 animateWork">
+          <img className="w-32" src={one} />
+          <h2 className="text-xl md:text-2xl font-bold ">
+            <span className="text-green_custom">Mattel</span>, Software Engineer
+            Intern
+          </h2>
+          <p className="text-sm md:text-base">
+            Created and maintained internal softwares using vb.net, electron.js,
+            asp.net mvc (c# and vb), python, ssms, microsoft power technologies,
+            etc.{" "}
+          </p>
+        </div>
+        <div className="flex flex-col gap-2 animateWork">
+          <img className="w-32" src={two} />
+          <h2 className="text-xl md:text-2xl  font-bold ">
+            <span className="text-blue_custom">Tanamin</span>, Co-founder and
+            mobile developer
+          </h2>
+          <p className="text-sm md:text-base">
+            Co-founded the startup and served as the mobile developer with
+            funding from both Google and the government, totaling IDR 140
+            million.
+          </p>
+        </div>
 
-          <div className=' py-10 flex flex-col lg:flex-row gap-16' ref={number}>
-            <div className='flex flex-col gap-2 animateWork' >
-              <img className='w-32' src='../../src/assets/numbers/01.png'/>
-              <h2 className='text-xl md:text-2xl font-bold '><span className='text-green_custom'>Mattel</span>, Software Engineer Intern</h2>
-              <p className='text-sm md:text-base'>Created and maintained internal softwares using vb.net, electron.js, asp.net mvc (c# and vb), python, ssms, microsoft power technologies, etc. </p>
-            </div>
-            <div className='flex flex-col gap-2 animateWork' >
-              <img className='w-32' src='../../src/assets/numbers/02.png'/>
-              <h2 className='text-xl md:text-2xl  font-bold '><span className='text-blue_custom'>Tanamin</span>, Co-founder and mobile developer</h2>
-              <p className='text-sm md:text-base'>Co-founded the startup and served as the mobile developer with funding from both Google and the government, totaling IDR 140 million.</p>
-            </div>
-            
-            <div className='flex flex-col gap-2 animateWork' >
-              <img className='w-32' src='../../src/assets/numbers/03.png'/>
-              <h2 className='text-xl md:text-2xl  font-bold '><span className='text-yellow_custom'>Bangkit Academy</span>, Android Learning path</h2>
-              <p className='text-sm md:text-base'>Selected from 63k+ applicants to join Bangkit Academy 2022 in the Mobile Development learning path. Graduated with distinction with score of 98.5.</p>
-            </div>
-          </div>
+        <div className="flex flex-col gap-2 animateWork">
+          <img className="w-32" src={three} />
+          <h2 className="text-xl md:text-2xl  font-bold ">
+            <span className="text-yellow_custom">Bangkit Academy</span>, Android
+            Learning path
+          </h2>
+          <p className="text-sm md:text-base">
+            Selected from 63k+ applicants to join Bangkit Academy 2022 in the
+            Mobile Development learning path. Graduated with distinction with
+            score of 98.5.
+          </p>
+        </div>
+      </div>
     </section>
-  )
+  );
 }
