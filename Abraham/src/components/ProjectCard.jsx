4febbda14e9c9f6324dcdfa04407d06fe420f123
Abraham Naiborhu <87258755+abrahamparn@users.noreply.github.com>
2023-11-10 21:03:53 +0700

    adding project template

--- Abraham/src/components/ProjectCard.jsx
+++ Abraham/src/components/ProjectCard.jsx
@@ -2,13 +2,13 @@ import React from "react";
 import abraham_radial from "../../src/assets/abraham_radial.png";
 export default function ProjectCard() {
   return (
-    <div className="bg-black_lighter">
+    <div className="bg-black_lighter ">
       <img src={abraham_radial} />
-      <div className="pb-6 pt-6 ps-5 pe-5 flex flex-col gap-2 font-sen">
-        <p className="text-xl text-white font-bold">
+      <div className="pb-6 pt-6 ps-5 pe-5 flex flex-col gap-2 xl:gap-4 xxl:gap-6 font-sen">
+        <p className="text-xl lg:text-2xl xl:text-3xl xxl:text-4xl text-white font-bold">
           Tanamin - Android Application
         </p>
-        <p className="text-light_gray">
+        <p className="text-light_gray lg:text-xl xl:text-2xl xxl:text-3xl">
           An android application that uses machine learning to detect plant
           diseases. I am using Kotlin to create this application.
         </p>
--- Abraham/src/parts/Home/Project.jsx
+++ Abraham/src/parts/Home/Project.jsx
@@ -1,20 +1,41 @@
-import React from 'react'
-import ProjectCard from '../../components/ProjectCard'
+import React from "react";
+import ProjectCard from "../../components/ProjectCard";
 
 export default function Project() {
   return (
-    <section className='p-5 bg-black ProjectCard'>
-        <div>
-            <p className='text-sm font-bold text-light_gray font-sen'>MY PROJECT</p>
-            <p className='text-3xl font-sen font-bold text-white text-left'>Projects that I'have done so far</p>
-        </div>
-        <ProjectCard/>
-        <ProjectCard/>
-        <ProjectCard/>
-        <ProjectCard/>
-        <ProjectCard/>
-        <ProjectCard/>
-        <ProjectCard/>
+    <section className="p-5 xxl:p-16 bg-black columns-1  space-y-7 md:columns-3 md:gap-5  xxl:columns-5 xxl:gap-16 md:space-y-10">
+      <div>
+        <p className="text-sm mb:text-base lg:text-lg xl:text-xl font-bold text-light_gray font-sen ">
+          MY PROJECT
+        </p>
+        <p className="text-3xl mb:text-4xl lg:text-5xl xl:text-6xl font-sen font-bold text-white text-left">
+          Projects that I'have done so far
+        </p>
+      </div>
+      <div className="break-inside">
+        <ProjectCard />
+      </div>
+      <div className="break-inside">
+        <ProjectCard />
+      </div>
+      <div className="break-inside">
+        <ProjectCard />
+      </div>
+      <div className="break-inside">
+        <ProjectCard />
+      </div>
+      <div className="break-inside">
+        <ProjectCard />
+      </div>
+      <div className="break-inside">
+        <ProjectCard />
+      </div>
+      <div className="break-inside">
+        <ProjectCard />
+      </div>
+      <div className="break-inside">
+        <ProjectCard />
+      </div>
     </section>
-  )
+  );
 }
